660fe0b654b5ad3f82990e37f51dd016a09d5e0b
Marco Martins <marcodanielmartins@gmail.com>
2022-09-14 20:26:26 +0200

    Move svelte example

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 export default defineConfig({
-    plugins: [vanillaExtractPlugin(), react()]
+    plugins: [vanillaExtractPlugin(), react(), svelte()]
 })
